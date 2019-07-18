a6e5a2a20fa48da317fc36b59eaf7d39eca947a6
Jason Xia <jason.xia@sap.com>
2019-07-18 13:43:45 +0800

    implement XMLMessageMappingHelper4Search with mock data

--- src/dataTransformer/XMLMessageMappingHelper4Search.ts
+++ src/dataTransformer/XMLMessageMappingHelper4Search.ts
@@ -1,7 +1,9 @@
 import { XmlDocument } from 'xmldoc';
+import { SearchResultsDTO } from '../dto/searchResults.dto';
 
 export class XMLMessageMappingHelper4Search {
-    map(source: XmlDocument): XmlDocument {
-        return null;
+    map(source: XmlDocument): SearchResultsDTO {
+        const mockAdapterSearchResults = require('../common/mockdata/adapterSearchResults.json');
+        return mockAdapterSearchResults;
     }
 }
